ec9446fc3ec2d7f9d5e3d99429e5a371d95c73cb
Alejandro <alejandro1996rd@gmail.com>
2020-02-13 12:43:07 +0100

    listado hecho

--- src/app/paginas/privado/privado.component.ts
+++ src/app/paginas/privado/privado.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/model/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-privado',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrivadoComponent implements OnInit {
 
-  constructor() { }
+  pokemon: Array<Pokemon>;
+
+  constructor(private servicioPokemon : PokemonService) {
+    console.trace('PrivadoComponent constructor');
+    this.pokemon= [];
+  }//constructor
 
   ngOnInit() {
-  }
+    console.trace('PrivadoComponent ngOnInit');
+    this.cargarTareas();
+  }//ngOnInit
+
+  
+  private cargarTareas(): void {
+    console.trace('cargarTareas');
+
+    // llamar al service para obtener tareas
+    this.servicioPokemon.getAll().subscribe(
+      datos => {
+        console.debug('esto se ejecuta de forma asincrona');
+        this.pokemon = datos;
+      },
+      error => {
+        console.warn('Servico Rest no funciona %o', error);
+      });
+
+  }// cargarTareas
 
 }
